e4f405eb6b8e6c50a09fb2a1ef31884d910db766
amesameh <amrsameh903@gmail.com>
2023-06-27 23:11:39 +0300

    Create task2.php

---
+++ task2.php
@@ -0,0 +1,49 @@
+<?php
+
+
+// what is the result of the below
+
+// 5+2*3 = 11
+// 5*2-3 = 7
+// (5+2)*3 = 21
+// 5*(2-3) //-5
+
+
+// $x = $y = $j = 10;
+
+// $x = 10 ;
+// $y = 20 ;
+// $j = 5 ;
+
+// $x*$y/$j
+//40
+
+// $x=true
+// $y=false
+// $x && !$y
+//true
+
+
+// $x= true
+// $y=false
+// $z=true
+// $x && !$y || $z && false 
+// false
+
+
+// $x = 10 && false
+// $x = false
+// $x = 10 || false
+// $x = 10
+// $x = 10 or false
+// $x = 10
+// $x = 10 and false
+// $x = false
+
+
+$family = ['Name' => 'Amr', 'Age' => '21', 'Address' => 'Fakous'];
+
+var_dump($family)  ;
+
+
+?>
